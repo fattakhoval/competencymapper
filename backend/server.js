903658004b4dc14af40715cf4b2e8606e967af4d
Denis18UKS <honorxpremium75@gmail.com>
2025-01-24 07:53:53 +0500

    Actual Version

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,14 @@ db.getConnection()
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Access token is missing' });
+    if (!token) {
+        return res.status(401).json({ message: 'Access token is missing' });
+    }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid or expired token' });
+        if (err) {
+            return res.status(403).json({ message: 'Invalid or expired token' });
+        }
         req.user = user; // Сохраняем информацию о пользователе в запросе
         next();
     });
